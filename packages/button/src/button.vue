2417fefce4c0e2cabb89aa94906697dcee1765f6
300code <techo@300code.net>
2026-02-15 01:50:04 +0300

    Implemented custom slot handling and fixed default slot behavior of elbutton

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -20,6 +20,7 @@
     <i class="el-icon-loading" v-if="loading"></i>
     <i :class="icon" v-if="icon && !loading"></i>
     <span v-if="$slots.default"><slot></slot></span>
+    <slot name="custom"></slot>
   </button>
 </template>
 <script>
